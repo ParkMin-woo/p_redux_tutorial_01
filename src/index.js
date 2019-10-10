9f05cfac1ff3f90cad1c355c039f07641f9be26d
Park Minwoo <lwmwpark3523@gmail.com>
2019-10-11 01:33:42 +0900

    Second Redux

--- src/index.js
+++ src/index.js
@@ -1,28 +1,35 @@
-import {createStore} from "redux";
+import {combineReducers, createStore} from "redux";
 
-const reducer = (state, action) => {
-	console.log("action in index.js : " , action);
-	if(action.type === "changeState") {
-		return action.payload.newState;
+const productReducer = (state = [], action) => {
+	return state;
+}
+
+const userReducer = (state = '', action) => {
+	if(action.type === "updateUser") {
+		return action.payload;
 	}
 	else {
-		return "State";
+		return state;
 	}
 }
 
-const store = createStore(reducer ,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
-
-console.log("store in index.js : " , store);
-console.log("store.getState() in index.js : " , store.getState());
-
-const action = {
-	type : 'changeState',
-	payload : {
-		newState : 'New State'
+const allReducers = combineReducers(
+	{
+		products : productReducer,
+		user : userReducer,
 	}
+);
+
+const store = createStore(allReducers , window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+console.log("store.getState()1 : " , store.getState());
+
+// user를 update하는 action을 dispatch
+const userUpdate = {
+	type : "updateUser",
+	payload : "KIM SOOYOUNG",
 };
 
-store.subscribe(() => console.log("store.getState() : " , store.getState()));
+store.subscribe(() => console.log("store.getState()2 : " , store.getState()));
 
-store.dispatch(action);
-console.log("store.getState() in index.js : " , store.getState());
+store.dispatch(userUpdate);
